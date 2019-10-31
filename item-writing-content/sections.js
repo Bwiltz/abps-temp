43a316ca8a48c5c7cdb2f5b38864ab2ec4a5b375
Brian Wiltz <bwiltz@scitent.com>
2019-10-31 14:58:52 -0400

    adds content for client review

--- item-writing-content/sections.js
+++ item-writing-content/sections.js
@@ -1,9 +1,9 @@
 /* eslint-env browser, jquery */
 const sections = [
     { slug: '01_course_introduction', completion: 'visited', title: '1 - Introduction' },
-    { slug: '02', completion: 'visited', title: '2 - Blood Bank Preparations'  },
-    { slug: '03', completion: 'visited', title: '3 - Section 2'  },
-    { slug: '04', completion: 'visited', title: '4 - Section 3'  },
+    { slug: '02', completion: 'visited', title: '2 - Section 1'  },
+    { slug: '03', completion: 'visited', title: '3 - Question 1'  },
+    { slug: '04', completion: 'visited', title: '4 - Question 2'  },
     { slug: '05_congratulations', completion: 'visited', title: '5 - Congratulations'  },
     { slug: '404', title: '404', hidden: true }
 ];
